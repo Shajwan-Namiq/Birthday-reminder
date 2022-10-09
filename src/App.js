ea9c4a75dc6dc9a0ec1eaf0ef75ca4d1b82dbb81
Shajwan <shajwannamiqsalh@gmail.com>
2022-10-09 22:50:58 +0300

    adding clear btn && fetching data fron json file  && style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
  
+import { useState } from 'react';
 import './App.css';
+import data from './data';
+import List from './List';
+
 
 function App() {
+const [people ,setPeople] = useState(data);
+  
     return (
-        <div className='bg-slate-500'>home</div>
+      <>
+        <main>
+          <div className=" ">
+            <div className=" ">
+              <span className="m-10 flex justify-center font-bold text-[#f5ebe0] text-2xl ">
+                {people.length} Birthday today
+                 
+                  <button
+                    className="bg-[#6d6875] py-2 px-10 rounded text-white "
+                    onClick={() => setPeople([])}
+                  >
+                    Clear All
+                  </button>
+                
+              </span>
+
+              <List people={people} />
+            </div>
+          </div>
+        </main>
+      </>
     );
 }
 
--- src/List.js
+++ src/List.js
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from "react";
 
-export default function List() {
+const List = ({ people }) => {
   return (
-    <div>List</div>
-  )
-}
+    <>  
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        
+ return (
+   <div key={id}>
+     <ul
+       class="my-5 mx-5 lg:mx-60 p-5   flex justify-center items-center bg-[#d4a373bc] 
+        transform hover:scale-90 
+            hover:bg-[#d5bdaf] transition 
+            ease-out duration-500"
+     >
+       <li class="pb-3 sm:pb-4">
+         <div class="flex items-center  space-x-10">
+           <div class="flex-shrink-0">
+             <img class="w-20 h-20 rounded-full" src={image} alt={name} />
+           </div>
+           <div class="flex-1 min-w-0">
+             <p class="text-lg font-medium text-gray-900 truncate dark:text-white">
+               {name}
+             </p>
+             <p class="text-sm text-gray-900 truncate ">{age} years</p>
+           </div>
+         </div>
+       </li>
+     </ul>
+   </div>
+ );
+      })}
+    </>
+  );
+};
+
+export default List;
